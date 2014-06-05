b4b798e637aa12f2c4b89ed355dd05f0706ced65
Mike Bowler <mbowler@GargoyleSoftware.com>
2014-06-05 18:54:28 -0400

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SpaceHotel::Application.config.secret_key_base = 'b07aeb7ebc6c3e7afda4dade54fb58299f9c1c14814260ba1fae026b7310265146869dfbe0efa42719a139a4a0707c43b9d13605095b73f22265dcb8d65f0e62'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SpaceHotel::Application.config.session_store :cookie_store, key: '_space-hotel_session'
