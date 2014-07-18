e5a5fc826b55de62d455b4908d7af799dcf777dc
Mike Bowler <mbowler@GargoyleSoftware.com>
2014-07-18 10:36:47 -0400

    Experimenting with asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(:default, Rails.env)
 
 module SpaceHotel
   class Application < Rails::Application
+    config.serve_static_assets = true
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -19,6 +21,5 @@ module SpaceHotel
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.serve_static_assets = true
   end
 end
